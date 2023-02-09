a42495f19ed41a31e243634da6b0c4f4883c4427
rizwanar6296 <rizwanar6296@gmail.com>
2023-02-09 23:22:51 +0530

    added try catch block serverside

--- server/controller/uploadController.js
+++ server/controller/uploadController.js
@@ -4,31 +4,47 @@ const WordExtractor = require("word-extractor");
 
 module.exports = {
   upload: async (req, res) => {
-    const filePath = path.join(__dirname, `../uploads/${req.file.filename}`);
-    const extractor = new WordExtractor();
-    const extractedText = await extractor.extract(filePath);
-    const saveExtractedText = new Upload({
-      fileName: req.file.filename,
-      orginalName: req.file.originalname,
-      text: extractedText._body,
-    });
-    const savedExtract = await saveExtractedText.save();
-    res.send(savedExtract);
+    try {
+      const filePath = path.join(__dirname, `../uploads/${req.file.filename}`);
+      const extractor = new WordExtractor();
+      const extractedText = await extractor.extract(filePath);
+      const saveExtractedText = new Upload({
+        fileName: req.file.filename,
+        orginalName: req.file.originalname,
+        text: extractedText._body,
+      });
+      const savedExtract = await saveExtractedText.save();
+      res.send(savedExtract);
+    } catch (error) {
+      console.log("error while uploading");
+    }
   },
   getAllDocuments: async (req, res) => {
-    const documents = await Upload.find();
-    res.json(documents);
+    try {
+      const documents = await Upload.find();
+      res.json(documents);
+    } catch (error) {
+      console.log("error while getting all documents");
+    }
   },
   downloadDocument: async (req, res) => {
-    const { docId } = req.params;
-    const document = await Upload.findById(docId);
-    const filePath = path.join(__dirname, `../uploads/${document.fileName}`);
-    res.download(filePath);
+    try {
+      const { docId } = req.params;
+      const document = await Upload.findById(docId);
+      const filePath = path.join(__dirname, `../uploads/${document.fileName}`);
+      res.download(filePath);
+    } catch (error) {
+      console.log("error while downloading the file");
+    }
   },
   search: async (req, res) => {
-    const { searchKey } = req.params;
-    const regex = new RegExp(searchKey, "i");
-    const documents = await Upload.find({ text: { $regex: regex } });
-    res.json(documents);
+    try {
+      const { searchKey } = req.params;
+      const regex = new RegExp(searchKey, "i");
+      const documents = await Upload.find({ text: { $regex: regex } });
+      res.json(documents);
+    } catch (error) {
+      console.log('error while searching')
+    }
   },
 };
